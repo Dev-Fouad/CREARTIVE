1ab889414719ae4ff20fb4f0e62785890beec54d
fouad ajayi <fouadajayi6@gmail.com>
2023-03-04 14:52:33 +0100

    Its gone

--- src/Components/Header/UI/Button.tsx
+++ src/Components/Header/UI/Button.tsx
@@ -2,12 +2,22 @@ import React ,{useState} from "react";
 import "../Styles.css";
 
 const Button = ({ text, bg }) => {
+
+    const [Color, color] = useState(true)
+
+    function handler(){
+        color(false)
+    }
+
+    if ( text == ) {
+        
+    }
     
 
-  let classes = "btn  Bt  Bn"
+  let classes = Color ? 'btn' : 'btn Bt'
 
   return <>
-      <button className={classes}>{text}</button>
+      <button className={classes} >{text}</button>
   </>
 };
 
